38c28ca67d0bed729b3e285e3968e78b87f69d2f
Sajinder Singh <sanj.sohal@gmail.com>
2025-07-27 18:58:10 +0530

    Checking firebase config

--- src/main/java/com/rabbitinfosystems/ticketingsystem/config/FirebaseConfig.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/config/FirebaseConfig.java
@@ -15,7 +15,7 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() throws Exception {
         String credentials = System.getenv("GOOGLE_CREDENTIALS");
-
+        System.out.println("Credentials: " + credentials);
         GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ByteArrayInputStream(credentials.getBytes(StandardCharsets.UTF_8)));
 
         FirebaseOptions options = FirebaseOptions.builder()
