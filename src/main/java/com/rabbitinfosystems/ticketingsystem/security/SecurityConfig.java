e8f7505655d28d4cce1b249ec01b7820df2ba3db
Sajinder Singh <sanj.sohal@gmail.com>
2025-07-27 21:58:28 +0530

    Removing Firebase to check if that is the problem

--- src/main/java/com/rabbitinfosystems/ticketingsystem/security/SecurityConfig.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.rabbitinfosystems.ticketingsystem.security;
 
-import com.google.firebase.auth.FirebaseToken;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,9 +24,6 @@ public class SecurityConfig {
                 .build();
     }
 
-    //@Bean
-    public FirebaseTokenFilter firebaseTokenFilter() {
-        return new FirebaseTokenFilter();
-    }
+
 
 }
