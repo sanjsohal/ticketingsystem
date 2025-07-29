06208208399610ed50c4627fa2cca35f3adce95f
Sajinder Singh <sanj.sohal@gmail.com>
2025-07-29 22:51:09 +0530

    Added firebase config

---
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/config/FirebaseConfig.java
@@ -0,0 +1,30 @@
+package com.rabbitinfosystems.ticketingsystem.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseAuth firebaseAuth() throws IOException {
+        String credentialsJson = System.getenv("GOOGLE_CREDENTIALS");
+
+        ByteArrayInputStream serviceAccount = new ByteArrayInputStream(credentialsJson.getBytes(StandardCharsets.UTF_8));
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        var apps = FirebaseApp.getApps();
+        return apps.isEmpty()
+                ? FirebaseAuth.getInstance(FirebaseApp.initializeApp(options))
+                : FirebaseAuth.getInstance(apps.get(0));
+    }
+}
