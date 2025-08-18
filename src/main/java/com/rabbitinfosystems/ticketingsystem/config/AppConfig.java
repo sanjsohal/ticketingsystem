bf8d48e57a71195975168f84b2b69f60d34cb788
Sajinder Singh <sanj.sohal@gmail.com>
2025-08-18 22:17:23 +0530

    Introduced EnvironmentFile

--- src/main/java/com/rabbitinfosystems/ticketingsystem/config/AppConfig.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/config/AppConfig.java
@@ -4,7 +4,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
