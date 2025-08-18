0849932ebadad57809e1f4cb89c2ef59936c2b3c
Sajinder Singh <sanj.sohal@gmail.com>
2025-08-18 13:30:39 +0530

    Update DataSourceLogger.java

--- src/main/java/com/rabbitinfosystems/ticketingsystem/service/DataSourceLogger.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/service/DataSourceLogger.java
@@ -3,16 +3,21 @@ package com.rabbitinfosystems.ticketingsystem.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Component
 public class DataSourceLogger implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(DataSourceLogger.class);
+    
     @Value("${spring.datasource.url}")
     private String datasourceUrl;
 
     @Override
     public void run(String... args) {
-        System.out.println("### SPRING DATASOURCE URL: " + datasourceUrl);
+        log.info("### SPRING DATASOURCE URL: " + datasourceUrl);
     }
 }
 
