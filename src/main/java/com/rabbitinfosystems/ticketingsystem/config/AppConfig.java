c45c0ec635feb2266a63c801d115fb947fc1e5d8
Sajinder Singh <sanj.sohal@gmail.com>
2025-08-23 23:43:38 +0530

    Fixing cyclic dependency

--- src/main/java/com/rabbitinfosystems/ticketingsystem/config/AppConfig.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/config/AppConfig.java
@@ -4,15 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.rabbitinfosystems.ticketingsystem.service.AwsSecretsService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerException;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
@@ -20,16 +21,16 @@ import java.nio.charset.StandardCharsets;
 @Configuration
 public class AppConfig {
 
-    private final AwsSecretsService awsSecretsService;
+    private final SecretsManagerClient secretsManagerClient;
 
-    public AppConfig(AwsSecretsService awsSecretsService) {
-        this.awsSecretsService = awsSecretsService;
+    public AppConfig(SecretsManagerClient secretsManagerClient) {
+        this.secretsManagerClient = secretsManagerClient;
     }
 
     @Bean
     @Profile("!local")
     public FirebaseApp firebaseApp() throws IOException {
-        String secretJson = awsSecretsService.getSecret("ticketing/firebase/service-account");
+        String secretJson = getSecret("ticketing/firebase/service-account");
 
         // Convert JSON string to stream
         ByteArrayInputStream serviceAccountStream =
@@ -39,9 +40,7 @@ public class AppConfig {
                 .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
                 .build();
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-        }
+        FirebaseApp.getApps();
         return FirebaseApp.initializeApp(options);
     }
 
@@ -56,6 +55,15 @@ public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    public String getSecret(String secretName) {
+        try {
+            GetSecretValueRequest valueRequest = GetSecretValueRequest.builder().secretId(secretName).build();
+            return secretsManagerClient.getSecretValue(valueRequest).secretString();
+        } catch(SecretsManagerException exception) {
+            throw new RuntimeException("Failed to retrieve secret: " + secretName, exception);
+        }
     }
 }
 
