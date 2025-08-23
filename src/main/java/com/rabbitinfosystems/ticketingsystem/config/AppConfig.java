7577e83f388e44cfc2f946f0cd3c0132c7fb5134
Sajinder Singh <sanj.sohal@gmail.com>
2025-08-23 22:30:42 +0530

    Adding firebase service account in secrets manager

--- src/main/java/com/rabbitinfosystems/ticketingsystem/config/AppConfig.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/config/AppConfig.java
@@ -1,34 +1,51 @@
 package com.rabbitinfosystems.ticketingsystem.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
+import com.rabbitinfosystems.ticketingsystem.service.FirebaseSecretService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.FileInputStream;
+
 
 @Configuration
 public class AppConfig {
 
     @Bean
     @Profile("!local")
-    public FirebaseAuth firebaseAuth() throws IOException {
-        String credentialsJson = System.getenv("GOOGLE_CREDENTIALS");
-        System.out.println("Debugging credentialsJson");
-        System.out.println(credentialsJson);
-        ByteArrayInputStream serviceAccount = new ByteArrayInputStream(credentialsJson.getBytes(StandardCharsets.UTF_8));
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-        var apps = FirebaseApp.getApps();
-        return apps.isEmpty()
-                ? FirebaseAuth.getInstance(FirebaseApp.initializeApp(options))
-                : FirebaseAuth.getInstance(apps.get(0));
+    public FirebaseApp firebaseApp(FirebaseSecretService firebaseSecretService) {
+        try {
+            FirebaseSecretService.FirebaseConfig config = firebaseSecretService.getFirebaseConfig();
+
+            FileInputStream serviceAccount = new FileInputStream("firebase-service-account.json");
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            return FirebaseApp.initializeApp(options);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize Firebase", e);
+        }
     }
 
+    @Bean
+    @Profile("!local")
+    public SecretsManagerClient secretsManagerClient() {
+        return SecretsManagerClient.builder()
+                .region(Region.AP_SOUTH_1)
+                .build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
+
