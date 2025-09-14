b8dbff2579f63c2a20611fb37437b59418f987f2
Sajinder Singh <sanj.sohal@gmail.com>
2025-09-15 00:04:45 +0530

    Making register endpoint public and adding a filter to check origin if register is only called through my front end

---
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/filter/OriginCheckFilter.java
@@ -0,0 +1,25 @@
+package com.rabbitinfosystems.ticketingsystem.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class OriginCheckFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if("/api/users/register".equals(request.getRequestURI())) {
+            String origin = request.getHeader("Origin");
+            if("https://modern-ticketing-system.vercel.app".equals(origin)) {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/rabbitinfosystems/ticketingsystem/security/SecurityConfig.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/security/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.rabbitinfosystems.ticketingsystem.security;
 
 import com.rabbitinfosystems.ticketingsystem.filter.FirebaseAuthenticationFilter;
+import com.rabbitinfosystems.ticketingsystem.filter.OriginCheckFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,14 +17,19 @@ public class SecurityConfig {
     @Autowired
     private FirebaseAuthenticationFilter firebaseAuthenticationFilter;
 
+    @Autowired
+    private OriginCheckFilter originCheckFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/api/users/register").permitAll()
                 .anyRequest().authenticated()
             )
-            .addFilterBefore(firebaseAuthenticationFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(originCheckFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(firebaseAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
