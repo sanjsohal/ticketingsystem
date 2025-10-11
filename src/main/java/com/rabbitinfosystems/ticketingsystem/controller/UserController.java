895445635eff3e7f144d8b6e3d8714fff0fd74af
Sajinder Singh <sanj.sohal@gmail.com>
2025-10-11 22:27:09 +0530

    Adding api prefix for all endpoints

--- src/main/java/com/rabbitinfosystems/ticketingsystem/controller/UserController.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
