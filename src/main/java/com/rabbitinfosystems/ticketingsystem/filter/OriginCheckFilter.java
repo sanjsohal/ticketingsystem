2bf13210130a01ee781f06917bcdf88d40c262fc
Sajinder Singh <sanj.sohal@gmail.com>
2025-10-11 21:30:44 +0530

    Bypassing firebase filter for public endpoint

--- src/main/java/com/rabbitinfosystems/ticketingsystem/filter/OriginCheckFilter.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/filter/OriginCheckFilter.java
@@ -7,20 +7,29 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 @Component
 public class OriginCheckFilter extends OncePerRequestFilter {
 
+    private static final Logger logger = LoggerFactory.getLogger(OriginCheckFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if ("/api/users/register".equals(request.getRequestURI())) {
             String origin = request.getHeader("Origin");
+            logger.debug("Origin header: {}", origin);
+
             if (!"https://modern-ticketing-system.vercel.app".equals(origin)) {
+                logger.debug("Blocking request due to mismatched Origin header");
                 response.setStatus(HttpServletResponse.SC_FORBIDDEN);
                 return;
             }
         }
+        logger.debug("Allowing request to proceed");
         filterChain.doFilter(request, response);
     }
 }
