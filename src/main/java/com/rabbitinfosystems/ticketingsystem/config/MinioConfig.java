1ba6d27c6414d297428b24f621e8267860c0a91e
Sajinder Singh <sanj.sohal@gmail.com>
2025-10-11 23:24:33 +0530

    Removing config that is not required

--- src/main/java/com/rabbitinfosystems/ticketingsystem/config/MinioConfig.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/config/MinioConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
@@ -16,7 +17,28 @@ import java.net.URI;
 public class MinioConfig {
 
     @Bean
+    public S3Client s3Client() {
+        log.debug("s3Client MINIO_ENDPOINT: {}", System.getenv("MINIO_ENDPOINT"));
+        log.debug("s3Client MINIO_STORAGE_ENDPOINT: {}", System.getenv("MINIO_STORAGE_ENDPOINT"));
+        log.debug("s3Client MINIO_ROOT_USER: {}", System.getenv("MINIO_ROOT_USER"));
+        log.debug("s3Client MINIO_ROOT_PASSWORD: {}", System.getenv("MINIO_ROOT_PASSWORD"));
+        return S3Client.builder()
+                .endpointOverride(URI.create(System.getenv("MINIO_ENDPOINT")))
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(
+                                System.getenv("MINIO_ROOT_USER"),
+                                System.getenv("MINIO_ROOT_PASSWORD"))
+                        ))
+                .build();
+    }
+
+    @Bean
     public S3Presigner s3Presigner() {
+        log.debug("s3Presigner MINIO_ENDPOINT: {}", System.getenv("MINIO_ENDPOINT"));
+        log.debug("s3Presigner MINIO_STORAGE_ENDPOINT: {}", System.getenv("MINIO_STORAGE_ENDPOINT"));
+        log.debug("s3Presigner MINIO_ROOT_USER: {}", System.getenv("MINIO_ROOT_USER"));
+        log.debug("s3Presigner MINIO_ROOT_PASSWORD: {}", System.getenv("MINIO_ROOT_PASSWORD"));
         S3Configuration s3Configuration = S3Configuration.builder()
                 .pathStyleAccessEnabled(true)
                 .build();
