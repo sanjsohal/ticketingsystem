cd2c284827da72b1c50444776a7d43938e12b559
Sajinder Singh <sanj.sohal@gmail.com>
2025-10-13 19:24:54 +0530

    Dropped column

--- src/main/java/com/rabbitinfosystems/ticketingsystem/dto/RegistrationPayload.java
+++ src/main/java/com/rabbitinfosystems/ticketingsystem/dto/RegistrationPayload.java
@@ -3,5 +3,5 @@ package com.rabbitinfosystems.ticketingsystem.dto;
 
 
 public record RegistrationPayload(String firebaseUid, String email,
-                                  String name, String avatar) {
+                                  String name) {
 }
